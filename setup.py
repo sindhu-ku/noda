28406913b9baa68ffe4121f12c2b023a28cdb4fc
sindhu-ku <kumaran.sindhujha@gmail.com>
2024-08-07 10:48:52 -0700

    make it a package

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='noda',
+    version='2.1.0',
+    description='Neutrino Oscillation Data Analysis: fitting framework for JUNO',
+    author='Sindhujha Kumaran',
+    author_email='s.kumaran@uci.edu',
+    packages = find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'run_noda = noda.main:main',
+        ],
+    },
+
+    install_requires=[
+        'matplotlib',
+        'scipy',
+        'joblib',
+        'numpy',
+        'pandas',
+        'emcee',
+        'ptemcee',
+        'xarray',
+        'arviz',
+        'corner',
+        'iminuit'
+    ],
+    python_requires='>=3.9',
+)
